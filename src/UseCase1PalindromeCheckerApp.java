c22bab62a62e3ffa3ccf375846b66d3b4408eb31
jigisha1933 <ja1933@srmist.edu.in>
2026-02-26 08:42:41 +0530

    UC1: Application entry and welcome message

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,22 @@
+/*
+ * UC1: Application Entry & Welcome Message
+ * Palindrome Checker App
+ * Version: 1.0
+ */
+
+public class UseCase1PalindromeCheckerApp {
+
+    // Main method – Entry point of JVM
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println("        PALINDROME CHECKER APP         ");
+        System.out.println("=======================================");
+        System.out.println("Application Version : 1.0");
+        System.out.println("Developed for       : Learning Purpose");
+        System.out.println("=======================================");
+        System.out.println("Application Started Successfully!");
+        System.out.println("=======================================");
+
+    }
+}
